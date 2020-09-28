d8e2520f5d075dd4ee08e0fb41db86f2357efc4b
Sanya Gupta <sanyagupta2001@yahoo.com>
2020-09-28 19:56:16 +0530

    Updated events button

--- lib/widgets/events_button.dart
+++ lib/widgets/events_button.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:events_page/constants.dart';
 
 class EventsButton extends StatefulWidget {
   final String eventName;
@@ -17,52 +16,112 @@ class _EventsButtonState extends State<EventsButton> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(vertical: 15),
+      padding: const EdgeInsets.symmetric(vertical: 10),
       child: Container(
         decoration: widget.eventTileStyle,
-        padding: EdgeInsets.symmetric(horizontal: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Icon(
-              widget.align == true ? Icons.arrow_back_ios : null,
-              color: Colors.white,
-            ),
-            Expanded(
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      widget.eventName,
-                      style: kEventTextStyle,
-                    ),
-                    Divider(
-                      color: kElementColor,
-                      height: 28,
-                      thickness: 1.5,
-                      // indent: 0.1 * MediaQuery.of(context).size.width,
-                      // endIndent: 0.08 * MediaQuery.of(context).size.width,
-                    ),
-                    Text(
-                      widget.eventCategory,
-                      style: kCategoryTextStyle,
-                    ),
-                  ],
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              widget.align == true
+                  ? CircleAvatar(
+                      radius: 30,
+                      backgroundColor: Color(0x10ffffff),
+                      child: Icon(
+                        Icons.arrow_back_ios,
+                        color: Colors.white30,
+                        size: 30,
+                      ),
+                    )
+                  : SizedBox.shrink(),
+              Column(children: [
+                Text(
+                  widget.eventName,
+                  style: TextStyle(fontSize: 20, fontFamily: 'Cabin'),
                 ),
+                SizedBox(
+                  height: 4,
+                ),
+                Text(
+                  "Category Name",
+                  style: TextStyle(fontSize: 10, fontFamily: 'Cabin'),
+                ),
+              ]),
+              SizedBox(
+                width: 20,
               ),
-            ),
-            Icon(
-              widget.align == false ? Icons.arrow_forward_ios : null,
-              color: Colors.white,
-            ),
-          ],
+              widget.align == true
+                  ? SizedBox.shrink()
+                  : CircleAvatar(
+                      radius: 30,
+                      backgroundColor: Color(0x10ffffff),
+                      child: Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.white30,
+                        size: 30,
+                      ),
+                    )
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
+// Padding(
+//   padding: EdgeInsets.symmetric(vertical: 15),
+//   child: Container(
+//     decoration: widget.eventTileStyle,
+//     padding: EdgeInsets.all(15),
+//     child: Row(
+//       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//       crossAxisAlignment: CrossAxisAlignment.center,
+//       children: <Widget>[
+//         Icon(
+//           widget.align == true ? Icons.arrow_back_ios : null,
+//           color: Colors.white,
+//           size: 20,
+//         ),
+//         Expanded(
+//           child: Padding(
+//             padding: widget.align == false
+//                 ? const EdgeInsets.symmetric(horizontal: 50, vertical: 10)
+//                 : const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+//             child: Text(
+//               widget.eventName,
+//               style: TextStyle(fontSize: 20),
+//             ),
+//             //Column(
+//             //   crossAxisAlignment: CrossAxisAlignment.center,
+//             //   mainAxisAlignment: MainAxisAlignment.center,
+//             //   children: <Widget>[
+//             //     Text(
+//             //       widget.eventName,
+//             //       style: TextStyle(fontSize: 20),
+//             //     ),
+//             //     Divider(
+//             //       color: kElementColor,
+//             //       height: 28,
+//             //       thickness: 1.5,
+//             //       // indent: 0.1 * MediaQuery.of(context).size.width,
+//             //       // endIndent: 0.08 * MediaQuery.of(context).size.width,
+//             //     ),
+//             //     Text(
+//             //       widget.eventCategory,
+//             //       style: kCategoryTextStyle,
+//             //     ),
+//             //   ],
+//             // ),
+//           ),
+//         ),
+//         Icon(
+//           widget.align == false ? Icons.arrow_forward_ios : null,
+//           color: Colors.white,
+//           size: 20,
+//         ),
+//       ],
+//     ),
+//   ),
+// )
